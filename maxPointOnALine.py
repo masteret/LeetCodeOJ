53fe9ad6fed7d7d2118bffdefda76f5c207db183
masteret <masteret9432@gmail.com>
2015-07-01 23:31:15 -0500

    initial push

--- maxPointOnALine.py
+++ maxPointOnALine.py
@@ -1,1 +1,11 @@
 __author__ = 'ET'
+
+# Definition for a point.
+# class Point:
+#     def __init__(self, a=0, b=0):
+#         self.x = a
+#         self.y = b
+
+# @param {Point[]} points
+# @return {integer}
+def maxPoints(self, points):
