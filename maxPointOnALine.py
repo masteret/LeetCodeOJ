1aec95608d1d7b5d96ab54c11d9dd19b2cf933a7
masteret <masteret9432@gmail.com>
2015-07-10 01:42:52 -0500

    Time limit exceed

--- maxPointOnALine.py
+++ maxPointOnALine.py
@@ -1,5 +1,5 @@
 __author__ = 'ET'
-
+import timeit
 # Definition for a point.
 class Point:
     def __init__(self, a=0, b=0):
@@ -13,55 +13,73 @@ class Point:
 # @return {integer}
 def maxPoints(points):
     if len(points) < 3:
-        return len(points)
+        return str(len(points))
 
     grad_list = {}
-    same_list = {}
-    for xcount, xpoint in enumerate(points[:-1]):
-        same = 0
+    # same_list = {}
+    for xcount, xpoint in enumerate(points):
+        # if same_list.get(xpoint) is None:
+        #     same_list.update({xpoint:1})
+        # else:
+        #     same_list.update({xpoint:same_list.get(xpoint)+1})
         for ypoint in points[xcount+1:]:
             if ypoint.x - xpoint.x == 0:
                 grad = -1.0
             else:
                 grad = (ypoint.y - xpoint.y)/(ypoint.x - xpoint.x)
 
-            if xpoint.x == ypoint.x and xpoint.y == ypoint.y:
-                if same_list.get(xpoint.tostring()) is None:
-                    same_list.update({xpoint.tostring():0})
-                else:
-                    same_list.update({xpoint.tostring():same_list.get(xpoint.tostring())+1})
-
-            if grad_list.get(grad) is None:
-                grad_list.update({grad:[[xpoint.tostring(), ypoint.tostring()]]})
+            current_grad = grad_list.get(grad)
+            if current_grad is None:
+                grad_list.update({grad:[[xpoint, ypoint]]})
             else:
                 inserted = False
-                for index, item in enumerate(grad_list.get(grad)):
-                    if xpoint.tostring() in item and ypoint.tostring() not in item:
-                        # print xpoint.tostring(), item
-                        grad_list.get(grad)[index] += [ypoint.tostring()]
+                for index, item in enumerate(current_grad):
+                    # print xpoint, ypoint, item
+                    if xpoint in item and ypoint not in item:
+                        current_grad[index] += [ypoint]
                         inserted = True
-                    elif xpoint.tostring() not in item and ypoint.tostring() in item:
-                        # print xpoint.tostring(), item
-                        grad_list.get(grad)[index] += [xpoint.tostring()]
+                    elif xpoint not in item and ypoint in item:
+                        current_grad[index] += [xpoint]
                         inserted = True
-                    elif xpoint.tostring() in item and ypoint.tostring() in item:
+                    elif xpoint in item and ypoint in item:
                         inserted = True
                 if not inserted:
-                    print xpoint.tostring(), ypoint.tostring(), "not inserted in", grad_list.get(grad)
-                    grad_list.update({grad:grad_list.get(grad).extend([xpoint.tostring(), ypoint.tostring()])})
+                    current_grad.append([xpoint, ypoint])
+                    # print xpoint, ypoint, "not inserted in", grad_list.get(grad)
 
-    for key, item in grad_list.items():
-        print key, item
-    for key, item in same_list.items():
-        print key, item
-    return None
+
+    # for same_key, same_item in same_list.items():
+    #     for grad_key, grad_items in grad_list.items():
+    #         for grad_index, grad_item in enumerate(grad_items):
+    #             if grad_item.count(same_key) > 0 and grad_item.count(same_key) < same_item:
+    #                 # print same_key, same_item, grad_item, grad_item.count(same_key)
+    #                 for x in range(0, same_item - grad_item.count(same_key)):
+    #                     grad_items[grad_index] += [same_key]
+    #                 grad_list.update({grad_key:grad_items})
+    #                 # print same_key, same_item, grad_item, grad_item.count(same_key)
+
+    result = 0
+    for grad_key, grad_items in grad_list.items():
+        for grad_item in grad_items:
+            # print grad_key,
+            # for indi_point in grad_item:
+                # print indi_point.tostring(),
+            result = max(result, len(grad_item))
+            # print
+
+    return result
 
 if __name__ == "__main__":
     # 5 0 3 2 3 3 4
-    print maxPoints([Point(1,2), Point(2,3), Point(3,3), Point(3,4), Point(4,5), Point(5,6)])
+    # print maxPoints([Point(1,2), Point(2,3), Point(3,3), Point(3,4), Point(4,5), Point(5,6)])
     # print maxPoints([])
     # print maxPoints([Point(0,0), Point(-1,-1), Point(2,2)])
     # print maxPoints([Point(0,0), Point(-1,-1), Point(1,-1)])
     # print maxPoints([Point(0,0), Point(-1,-1), Point(0,0)])
     # print maxPoints([Point(1,1), Point(1,1), Point(1,1)])
     # print maxPoints([Point(1,1), Point(1,1), Point(2,2), Point(2,2)])
+    # print maxPoints([Point(0,0), Point(1,1), Point(1,0), Point(2,1)])
+    start = timeit.default_timer()
+    print maxPoints([Point(40,-23),Point(9,138),Point(429,115),Point(50,-17),Point(-3,80),Point(-10,33),Point(5,-21),Point(-3,80),Point(-6,-65),Point(-18,26),Point(-6,-65),Point(5,72),Point(0,77),Point(-9,86),Point(10,-2),Point(-8,85),Point(21,130),Point(18,-6),Point(-18,26),Point(-1,-15),Point(10,-2),Point(8,69),Point(-4,63),Point(0,3),Point(-4,40),Point(-7,84),Point(-8,7),Point(30,154),Point(16,-5),Point(6,90),Point(18,-6),Point(5,77),Point(-4,77),Point(7,-13),Point(-1,-45),Point(16,-5),Point(-9,86),Point(-16,11),Point(-7,84),Point(1,76),Point(3,77),Point(10,67),Point(1,-37),Point(-10,-81),Point(4,-11),Point(-20,13),Point(-10,77),Point(6,-17),Point(-27,2),Point(-10,-81),Point(10,-1),Point(-9,1),Point(-8,43),Point(2,2),Point(2,-21),Point(3,82),Point(8,-1),Point(10,-1),Point(-9,1),Point(-12,42),Point(16,-5),Point(-5,-61),Point(20,-7),Point(9,-35),Point(10,6),Point(12,106),Point(5,-21),Point(-5,82),Point(6,71),Point(-15,34),Point(-10,87),Point(-14,-12),Point(12,106),Point(-5,82),Point(-46,-45),Point(-4,63),Point(16,-5),Point(4,1),Point(-3,-53),Point(0,-17),Point(9,98),Point(-18,26),Point(-9,86),Point(2,77),Point(-2,-49),Point(1,76),Point(-3,-38),Point(-8,7),Point(-17,-37),Point(5,72),Point(10,-37),Point(-4,-57),Point(-3,-53),Point(3,74),Point(-3,-11),Point(-8,7),Point(1,88),Point(-12,42),Point(1,-37),Point(2,77),Point(-6,77),Point(5,72),Point(-4,-57),Point(-18,-33),Point(-12,42),Point(-9,86),Point(2,77),Point(-8,77),Point(-3,77),Point(9,-42),Point(16,41),Point(-29,-37),Point(0,-41),Point(-21,18),Point(-27,-34),Point(0,77),Point(3,74),Point(-7,-69),Point(-21,18),Point(27,146),Point(-20,13),Point(21,130),Point(-6,-65),Point(14,-4),Point(0,3),Point(9,-5),Point(6,-29),Point(-2,73),Point(-1,-15),Point(1,76),Point(-4,77),Point(6,-29)])
+
+    print timeit.default_timer() - start
