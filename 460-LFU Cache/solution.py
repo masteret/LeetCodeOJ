e27dbdace957601012b491d84c3fb889f69f40eb
masteret <masteret9432@gmail.com>
2016-12-01 01:14:04 -0600

    gee, time limit exceed

--- 460-LFU Cache/solution.py
+++ 460-LFU Cache/solution.py
@@ -1,3 +1,5 @@
+import collections
+
 class LFUCache(object):
     def __init__(self, capacity):
         """
@@ -8,12 +10,6 @@ class LFUCache(object):
         self.recent = []
         self.capacity = capacity
 
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            result = func(*args, **kwargs)
-            print "cache: ", result.cache, " used count: ", result.used_count, " recent: ", result.recent
-        return wrapper
-
     def get(self, key):
         """
         :type key: int
@@ -21,19 +17,36 @@ class LFUCache(object):
         """
         if str(key) in self.cache:
             # increase used count
-
+            self.used_count[str(key)] += 1
             # move to recent
+            self.recent.pop(self.recent.index(key))
+            self.recent.append(key)
+
             return self.cache[str(key)]
         else:
             return -1            
 
-    @decorator
     def set(self, key, value):
         """
         :type key: int
         :type value: int
         :rtype: void
         """
+        if self.capacity == 0:
+            return None
+
+        # invalidate least use cache
+        if len(self.recent) == self.capacity and key not in self.recent:
+            sorted_used_count = collections.Counter(self.used_count).most_common()[::-1]
+            # find the least 
+            least_commons = [k for k, v in sorted_used_count if v == sorted_used_count[0][1]]
+            least_recent = min([self.recent.index(int(x)) for x in least_commons if int(x) in self.recent])
+            poped = str(self.recent[least_recent])
+
+            del self.cache[poped]
+            del self.used_count[poped]
+            self.recent.pop(self.recent.index(int(poped)))
+
         # insert to cache
         self.cache[str(key)] = value
         # increased used count
@@ -43,33 +56,31 @@ class LFUCache(object):
         if key in self.recent:
             self.recent.pop(self.recent.index(key))
             self.recent.append(key)
-        # pop others
-        elif len(self.recent) == self.capacity:
-            poped = self.recent.pop(0)
-            del self.cache[str(poped)]
-            del self.used_count[str(poped)]
-            self.recent.append(key)
         # no need to pop
         else:
             self.recent.append(key)
-        # invalidate least use cache
-            # self.cache
-            # used count
-        return self
 
+# obj = LFUCache(2)
+# obj.set(1, 1)
+# obj.set(2, 2)
+# print obj.get(1)
+# obj.set(3, 3)
+# print obj.get(2)
+# print obj.get(3) 
+# obj.set(4, 4) 
+# print obj.get(1)
+# print obj.get(3)
+# print obj.get(4) 
 
-# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(0)
+# obj.set(0, 0)
+# print obj.get(0)
+
 obj = LFUCache(2)
-# param_1 = obj.get(key)
-# obj.set(key,value)
-
-obj.set(1, 1)
-obj.set(2, 2)
-print obj.get(1)
-obj.set(3, 3)
 print obj.get(2)
-print obj.get(3) 
-obj.set(4, 4) 
+print obj.set(2,6)
 print obj.get(1)
-print obj.get(3)
-print obj.get(4) 
+print obj.set(1,5)
+print obj.set(1,2)
+print obj.get(1)
+print obj.get(2)
