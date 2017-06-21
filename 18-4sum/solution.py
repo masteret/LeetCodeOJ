b04d0e317f038998703e98fb6011f617909e2490
masteret <masteret9432@gmail.com>
2017-06-21 02:15:49 -0500

    4sum reduced to 3sum

--- 18-4sum/solution.py
+++ 18-4sum/solution.py
@@ -1,28 +1,50 @@
 class Solution(object):
-    def helper(self, nums, target, count):
-        if count == 1:
-            result = []
-            for i in nums:
-                if i == target:
-                    result.append([i])
-            return result
-        else:
-            result = []
-            for i in range(len(nums)):
-                tmp_nums = list(nums)
-                tmp_value = tmp_nums.pop(i)
-                tmp_target = target - tmp_value
-                tmp_result = self.helper(tmp_nums, tmp_target, count-1)
-                # print "****************************"
-                # print nums, target, count
-                # print tmp_nums, tmp_target, count-1
-                # print tmp_result
-                for inner in tmp_result:
-                    tmp_list = [tmp_value]
-                    if None not in inner:
-                        tmp_list.extend(inner)
-                        result.append(tmp_list)
-            return result
+    def twoSum(self, nums, target):
+        # build a 2 sum solver that generate non-duplicate result
+        # left and right pointer points to value, if added up, append result
+        # move pointers to next non-duplicate value
+        # repeat 2 and 3
+        # if doesn't add up, move left pt if smaller than target and move right pt if larger than target
+        # output all result until l > r
+        l = 0
+        r = len(nums)-1
+        result = []
+        while l < r:
+            if nums[l] + nums[r] < target:
+                l += 1
+            elif nums[l] + nums[r] > target:
+                r -= 1
+            else:
+                # we found the target!
+                result.append([nums[l], nums[r]])
+                while l < r and nums[l] == nums[l+1]:
+                    l += 1
+                while l < r and nums[r] == nums[r-1]:
+                    r -= 1
+                l += 1
+                r -= 1
+        return result
+        
+
+    def threeSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        # sorted the nums
+        # for any num in nums that isn't duplicate with the previous one, do a 2 sums with the remaining list and target = -num
+        # if there is a list of solution, propagate it with your current num
+        result = []
+        for i in range(len(nums)-2):
+            if nums[i] == nums[i-1] and i > 0:
+                continue
+            tmp_result = self.twoSum(nums[i+1:], target-nums[i])
+            if tmp_result != []:
+                for x in tmp_result:
+                    tmp = [nums[i]]
+                    tmp.extend(x)
+                    result.append(tmp)
+        return result
 
 
     def fourSum(self, nums, target):
@@ -31,7 +53,18 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        return [list(y) for y in set(tuple(sorted(x)) for x in self.helper(nums, target, 4))]
+        nums = sorted(nums)
+        result = []
+        for i in range(len(nums)-3):
+            if nums[i] == nums[i-1] and i > 0:
+                continue
+            tmp_result = self.threeSum(nums[i+1:], target-nums[i])
+            if tmp_result != []:
+                for x in tmp_result:
+                    tmp = [nums[i]]
+                    tmp.extend(x)
+                    result.append(tmp)
+        return result
 
 a = Solution()
 print a.fourSum([1, 0, -1, 0, -2, 2], 0)
