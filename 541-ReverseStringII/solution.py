e27afc5e35ee9b746aa7d530484f4967b761f105
masteret <masteret9432@gmail.com>
2017-06-21 23:01:55 -0500

    damn...

--- 541-ReverseStringII/solution.py
+++ 541-ReverseStringII/solution.py
@@ -5,3 +5,16 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
+        if len(s) <= k:
+            return s[::-1]
+        elif len(s) <= 2*k:
+            return s[:k][::-1] + s[k:]
+        else:
+            result = ""
+            for x in range(0, len(s), 2*k):
+                result = result + s[x:x+k][::-1] + s[x+k:x+2*k]
+            return result
+
+a = Solution()
+print a.reverseStr("abcdefg", 2)
+print a.reverseStr("abcd", 2)
