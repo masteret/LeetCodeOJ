4336d14b7e8c01e6983d55f28c45a10b8a6f724e
masteret <masteret9432@gmail.com>
2018-02-07 21:34:17 -0600

    hoho

--- 167-TwoSumII-Inputarrayissorted/sol.py
+++ 167-TwoSumII-Inputarrayissorted/sol.py
@@ -5,3 +5,13 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        s = 0
+        t = len(numbers)-1
+        while s < t:
+            if numbers[s]+numbers[t] == target:
+                return [s+1, t+1]
+            elif numbers[s]+numbers[t] > target:
+                t -= 1
+            else:
+                s += 1
+        return []
